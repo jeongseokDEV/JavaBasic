df65bb5d0b2bd978ecb0664408e3a51e14995379
kimjeongseok <telmsla@naver.com>
2020-04-03 19:28:17 +0900

    MathRandom을 사용한 로또 프로그램

---
+++ JavaExample/src/LottoEx1.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class LottoEx1 {
+	public static void main(String[] args)
+	{
+		int lotto[] = new int[6];
+		for(int i = 0; i < lotto.length; i++)
+		{
+			lotto[i] = (int)(Math.random()*45) +1;
+			for(int j = 0; j < i; j++)
+			{
+				if(lotto[i] == lotto[j])
+				{
+					i--;
+					break;
+				}
+			}
+		}
+		Arrays.sort(lotto);
+		System.out.print("당첨 번호는 = ");
+		for(int i = 0; i<lotto.length; i++)
+		{
+			System.out.print("["+lotto[i]+"]");
+		}
+		System.out.println("입니다.");
+	}
+}
